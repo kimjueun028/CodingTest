13313b5968d69ff6a546eddddea0998f0e05b003
kimjueun028 <92249116+kimjueun028@users.noreply.github.com>
2024-10-02 22:01:16 +0900

    [level 2] Title: 짝지어 제거하기, Time: 85.29 ms, Memory: 12.3 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,4 +1,18 @@
 def solution(s):
+    # new_s = list(s)
+    # i = 0
+    # len_s = len(s)
+    # while i <= (len_s-2):
+    #     n = new_s[i]
+    #     m = new_s[i+1]
+    #     if n == m:
+    #         new_s.remove(n)
+    #         new_s.remove(m)
+    #         i = 0
+    #         len_s = len(new_s)
+    #         continue
+    #     i += 1
+    # return 0 if len(new_s) else 1
     stack = []
     for i in s:
         if stack and i == stack[-1]:
@@ -7,19 +21,9 @@ def solution(s):
             stack.append(i)
     return 0 if stack else 1
 
+
+
+
+
 # 틀림
-#     new_s = list(s)
     
-#     i = 0
-#     len_s = len(s)
-#     while i <= (len_s-2):
-#         n = new_s[i]
-#         m = new_s[i+1]
-#         if n == m:
-#             new_s.remove(n)
-#             new_s.remove(m)
-#             i = 0
-#             len_s = len(new_s)
-#             continue
-#         i += 1
-#     return 0 if len(new_s) else 1
