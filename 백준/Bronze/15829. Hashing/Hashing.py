4e6263ce1638b2d32ae6d638fdebab6f64265da8
kimjueun028 <92249116+kimjueun028@users.noreply.github.com>
2025-01-17 11:50:47 +0900

    [Bronze II] Title: Hashing, Time: 40 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -4,12 +4,8 @@ user_input = input()
 
 N = int(user_input[0])
 line = user_input[1].strip()
-
-temp = []
-for i in line:
-    temp.append(ord(i)-96)
-
-result=0
-for index, value in enumerate(temp):
-    result += value * 31**(index)
-print(result)
+M = 1234567891
+result = 0
+for index, value in enumerate(line):
+    result += (ord(value)-96)*31**index
+print(result % M)
